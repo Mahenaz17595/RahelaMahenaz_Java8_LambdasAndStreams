8ab34da035216b3ad622dd5c0865138e66990a3f
17wh1a0595 <17wh1a0595@bvrithyderabad.edu.in>
2020-07-11 23:54:49 +0530

    files uploaded

---
+++ Lambdas_Streams/src/main/java/com/epam/AverageOfList.java
@@ -0,0 +1,27 @@
+package com.epam;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class AverageOfList {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no. of elements:");
+		int n = sc.nextInt();
+		ArrayList<Integer> list = new ArrayList<>();
+		System.out.println("Enter numbers of the list:");
+		sc.nextLine();
+		for(int i = 0;i < n;i++){ 
+			int s = sc.nextInt();
+			list.add(s);
+		}
+		
+		int sum = list.stream().mapToInt(i -> i.intValue()).sum();
+		double avg=0;
+		if(list.size() > 0) {
+			 avg = sum / list.size();
+		}
+		System.out.println("Average of numbers in the list:"+avg);
+	}
+	
+}
